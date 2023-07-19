6a6aa50765e7793361c08ba27722538b075ece60
nadiaGincoff <nadia.gincoff@gmail.com>
2023-07-20 01:01:51 +0200

    first commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+import avatar from '/avatar.png';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+export default function Nav() {
+  return (
+    <nav>
+      <h1>Logo</h1>
+    </nav>
+  )
+}

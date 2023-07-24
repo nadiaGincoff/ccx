ddd23c9ffe58c486c4f442b89daf82a855f51571
nadiaGincoff <nadia.gincoff@gmail.com>
2023-07-24 21:16:46 +0200

    deploy to vercel

---
+++ .vercel/output/functions/render.func/chunks/_release_@_@astro.e0118299.mjs
@@ -0,0 +1,9 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.4b38ff7d.mjs';
+import 'react';
+import 'react-dom/server.browser';
+
+const page = () => import('./pages/_release_.astro.fbc906de.mjs').then(n => n._);
+
+export { page };
---
+++ .vercel/output/functions/render.func/chunks/index@_@astro.1bd9a0d7.mjs
@@ -0,0 +1,9 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.4b38ff7d.mjs';
+import 'react';
+import 'react-dom/server.browser';
+
+const page = () => import('./pages/index.astro.a7897c7e.mjs');
+
+export { page };
